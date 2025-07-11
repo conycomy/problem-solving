c56e52365791dfa70bec093fe4529bffa141a49a
LimGyeongHan <91595140+conycomy@users.noreply.github.com>
2025-07-11 16:39:24 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,12 +1,17 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        if (is_prefix.length() > my_string.length()) 
-            return 0;
-        
-        if (my_string.startsWith(is_prefix)) {
-            return 1;
-        } else {
+        // 1. 길이 체크
+        if (is_prefix.length() > my_string.length()) {
             return 0;
         }
+
+        // 2. 문자 하나씩 비교
+        for (int i = 0; i < is_prefix.length(); i++) {
+            if (my_string.charAt(i) != is_prefix.charAt(i)) {
+                return 0;  // 하나라도 다르면 접두사 아님
+            }
+        }
+
+        return 1; // 전부 다 같으면 접두사 맞음
     }
 }
