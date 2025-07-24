0455e7ae52f1890770210d5c4d6881846e83a9e6
LimGyeongHan <91595140+conycomy@users.noreply.github.com>
2025-07-24 16:07:42 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        int general = hp / 5;
+        hp %= 5;
+        
+        int soldier = hp / 3;
+        hp %= 3;
+        
+        int worker = hp;
+        
+        return answer + general + soldier + worker;
+    
+        
+    }
+}
