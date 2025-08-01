251d060b208a93cda1e2c87052c417695fdd7304
LimGyeongHan <91595140+conycomy@users.noreply.github.com>
2025-08-02 00:36:09 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        String str = Integer.toString(num);
+        for(int i = 0; i < str.length(); i++){
+            if( k == str.charAt(i) - '0'){
+              return i + 1;
+            }
+        
+        }
+        return -1;
+    }
+}
