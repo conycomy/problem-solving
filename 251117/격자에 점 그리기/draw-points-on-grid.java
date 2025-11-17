5e28fd377633d876eae172812ac22c0e43751598
LimGyeongHan <91595140+conycomy@users.noreply.github.com>
2025-11-17 20:08:10 +0900

    Title: 격자에 점 그리기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251117/격자에 점 그리기/draw-points-on-grid.java
+++ 251117/격자에 점 그리기/draw-points-on-grid.java
@@ -3,13 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(Systme.in);
+        Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        
+        boolean[][] hasCoin = new boolean[N][N]; 
+
+        for(int i = 0; i < M; i++) {
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+            hasCoin[r][c] = true; 
+
+        }
+
+        int count = 1;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++){
+                if(hasCoin[i][j] == true){
+                    System.out.print(count + " ");
+                    count++;
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
         
 
+         
+
+        
+        
     }
 }
