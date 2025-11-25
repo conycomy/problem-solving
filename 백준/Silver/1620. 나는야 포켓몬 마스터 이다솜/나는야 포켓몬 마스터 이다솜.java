682efd3722fc40da4a41a6f9020fc19127cedccc
LimGyeongHan <91595140+conycomy@users.noreply.github.com>
2025-11-25 16:03:56 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 2272 ms, Memory: 335360 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -5,31 +5,31 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // 포켓몬의 개수 N
-        int M = sc.nextInt(); // 내가 맞춰야 하는 문제의 개수 M
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
         HashMap<Integer, String> pokemonBible = new HashMap<>();
         HashMap<String, Integer> oppositeBible = new HashMap<>();
+
         sc.nextLine();
 
-
-        for(int i = 1; i <= N; i++){
+        for(int i = 1; i <= N; i++) {
             String name = sc.nextLine();
+
             pokemonBible.put(i, name);
             oppositeBible.put(name, i);
         }
 
         for(int i = 0; i < M; i++){
             String question = sc.nextLine();
-            
-            if(question.matches("\\d+")) {
+
+            try {
                 int num = Integer.parseInt(question);
                 System.out.println(pokemonBible.get(num));
-            } else {
+            } catch(NumberFormatException e){
                 System.out.println(oppositeBible.get(question));
             }
         }
-
 
     }
 }
